8f0bb454db2b13e1f882170f21f696894fd5ed73
unknown <skrillex55214@gmail.com>
2022-08-02 19:18:35 +0300

    Fixed slick slider

--- js/slider.js
+++ js/slider.js
@@ -1,39 +1,40 @@
 
 $(document).ready(function(){
-    $('.main__slider').slick({
-        arrows: true,
-        dots:true,
-        adaptiveHeight: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        speed: 1000,
-        easing: '_linear',
-        initialSlide: 0,
-        autoplay:false,
-        autoplaySpeed: 10000,
-        pauseOnHover: false,
-        variableWidth:true,
-        infinite: false,
-        arrows:true,
-        appendArrows: $('.main__arrows'),
-        responsive:[
-            {
-                breakpoint: 1150,
-                settings:{
-                    slidesToShow: 3,
-                    slidesToScroll: 3}
-            },{
-                breakpoint: 880,
-                settings:{
-                    slidesToShow: 2,
-                    slidesToScroll: 2}
-            },{
-                breakpoint: 610,
-                settings:{
-                    slidesToShow: 1,
-                    slidesToScroll: 1}
-            }
-        ]
+    $( '.main__slider' ).each( function() {
+        $( this ).slick( {
+            arrows: true,
+            dots:true,
+            adaptiveHeight: true,
+            slidesToShow: 5,
+            slidesToScroll: 4,
+            speed: 1000,
+            easing: '_linear',
+            initialSlide: 0,
+            autoplay:false,
+            autoplaySpeed: 10000,
+            pauseOnHover: false,
+            variableWidth:true,
+            infinite: false,
+            arrows:true,
+            appendArrows: $(this).parents('.main__slider-container').find('.main__arrows'),
+            responsive:[
+                {
+                    breakpoint: 1150,
+                    settings:{
+                        slidesToShow: 4,
+                        slidesToScroll: 3}
+                },{
+                    breakpoint: 880,
+                    settings:{
+                        slidesToShow: 3,
+                        slidesToScroll: 2}
+                },{
+                    breakpoint: 610,
+                    settings:{
+                        slidesToShow: 2,
+                        slidesToScroll: 1}
+                }
+            ]
+        } );
     });
-    
 });
